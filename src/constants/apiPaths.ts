5cc1ce70389389b303dcb0486cb2e1eab772e3ee
gogicha007 <gogicha@gmail.com>
2026-05-14 09:38:12 +0400

    refactor: updated API_PATHS import property with relevant Import Service aws api gateway link

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,7 +1,7 @@
 const API_PATHS = {
   product: "https://pggxwmuiii.execute-api.eu-north-1.amazonaws.com",
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  import: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  import: "https://aetbrujo4m.execute-api.eu-north-1.amazonaws.com",
   bff: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
 };
