570bf830e3562d3d50f05e0fa0b307f2778bef00
gogicha007 <gogicha@gmail.com>
2026-06-02 22:53:26 +0400

    revise: api paths for cart and order

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,9 +1,9 @@
 const API_PATHS = {
   product: "https://pggxwmuiii.execute-api.eu-north-1.amazonaws.com",
-  order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  order: "https://snxmgkzni76rii3ymtg3moj4w40ergmx.lambda-url.eu-north-1.on.aws/api/profile/cart",
   import: "https://aetbrujo4m.execute-api.eu-north-1.amazonaws.com",
   bff: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  cart: "https://snxmgkzni76rii3ymtg3moj4w40ergmx.lambda-url.eu-north-1.on.aws/api",
 };
 
 export default API_PATHS;
