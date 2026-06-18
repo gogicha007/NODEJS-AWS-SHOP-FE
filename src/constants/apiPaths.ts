1fdf892f66848635a55c8e08b8c9138231a89c55
gogicha007 <gogicha@gmail.com>
2026-06-18 09:17:14 +0400

    refactor: add bff aws link to bff constant

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -2,7 +2,7 @@ const API_PATHS = {
   product: "https://pggxwmuiii.execute-api.eu-north-1.amazonaws.com",
   order: "https://snxmgkzni76rii3ymtg3moj4w40ergmx.lambda-url.eu-north-1.on.aws/api/profile/cart",
   import: "https://aetbrujo4m.execute-api.eu-north-1.amazonaws.com",
-  bff: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  bff: "http://gogicha007-cart-api-prod.eu-north-1.elasticbeanstalk.com",
   cart: "https://snxmgkzni76rii3ymtg3moj4w40ergmx.lambda-url.eu-north-1.on.aws/api",
 };
 
