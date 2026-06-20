b19b680eb9c589d551dce9fbaafb6b548a152ec3
gogicha007 <gogicha@gmail.com>
2026-06-20 18:19:00 +0400

    fix: cart link

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -3,7 +3,7 @@ const API_PATHS = {
   order: "https://snxmgkzni76rii3ymtg3moj4w40ergmx.lambda-url.eu-north-1.on.aws/api/profile/cart",
   import: "https://aetbrujo4m.execute-api.eu-north-1.amazonaws.com",
   bff: "http://gogicha007-cart-api-prod.eu-north-1.elasticbeanstalk.com",
-  cart: "http://gogicha007-cart-api-prodv7.eu-north-1.elasticbeanstalk.com/api",
+  cart: "https://gogicha007-cart-api-prodv7.eu-north-1.elasticbeanstalk.com/api",
 };
 
 export default API_PATHS;
