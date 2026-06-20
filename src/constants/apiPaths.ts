ef65efbcd6e2f4d9694f8604d63e53be2e4d7b78
gogicha007 <gogicha@gmail.com>
2026-06-20 18:20:03 +0400

    fix: bff link

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -2,7 +2,7 @@ const API_PATHS = {
   product: "https://pggxwmuiii.execute-api.eu-north-1.amazonaws.com",
   order: "https://snxmgkzni76rii3ymtg3moj4w40ergmx.lambda-url.eu-north-1.on.aws/api/profile/cart",
   import: "https://aetbrujo4m.execute-api.eu-north-1.amazonaws.com",
-  bff: "http://gogicha007-cart-api-prod.eu-north-1.elasticbeanstalk.com",
+  bff: "https://gogicha007-bff-api-env.eu-north-1.elasticbeanstalk.com",
   cart: "https://gogicha007-cart-api-prodv7.eu-north-1.elasticbeanstalk.com/api",
 };
 
