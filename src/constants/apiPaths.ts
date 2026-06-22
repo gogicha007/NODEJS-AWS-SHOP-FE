c9d31dbeb0f07872bbf0f2a3981da44bc0f5e10d
gogicha007 <gogicha@gmail.com>
2026-06-22 17:04:09 +0400

    fix: link for orders

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,6 +1,6 @@
 const API_PATHS = {
   product: "https://pggxwmuiii.execute-api.eu-north-1.amazonaws.com",
-  order: "https://snxmgkzni76rii3ymtg3moj4w40ergmx.lambda-url.eu-north-1.on.aws/api/profile/cart",
+  order: "https://gogicha007-cart-api-prodv7.eu-north-1.elasticbeanstalk.com/api/profile/cart",
   import: "https://aetbrujo4m.execute-api.eu-north-1.amazonaws.com",
   bff: "https://gogicha007-bff-api-env.eu-north-1.elasticbeanstalk.com",
   cart: "https://gogicha007-cart-api-prodv7.eu-north-1.elasticbeanstalk.com/api",
